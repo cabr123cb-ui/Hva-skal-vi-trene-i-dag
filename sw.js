6c43fd3e0e6d50f8d0e5efb3e0047836a30e5d12
cabr123cb-ui <cabr123.cb@gmail.com>
2026-09-23 10:51:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-const C='v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='v2';
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
